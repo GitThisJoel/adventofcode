9f544ac4e802dfe90a52fc24b7d3222e83599286
GitThisJoel <joelbacker10@gmail.com>
2023-12-10 13:47:21 +0100

    update: template

--- templates/template.py
+++ templates/template.py
@@ -10,16 +10,21 @@ def p1(data):
     lines = get_lines(data)
     chunks = get_chunks(data)
 
-    return
+    ans = 0
+    return 0
 
 
 def p2(data):
-    return
+    lines = get_lines(data)
+    chunks = get_chunks(data)
+
+    ans = 0
+    return 0
 
 
 if __name__ == "__main__":
     data = read_file(f"ins/1337.in")
-    # data = read_file(f"samples/1337.in")
+    data = read_file(f"samples/1337.in")
 
     print(f"part 1: {p1(data)}")
-    # print(f"part 2: {p2(data)}")
+    print(f"part 2: {p2(data)}")
