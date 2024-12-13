b96799965359a1f00c7c8ab0472bec30841de8e8
GitThisJoel <joelbacker10@gmail.com>
2024-12-13 09:25:49 +0100

    day 13 done

---
+++ 2024/13.py
@@ -0,0 +1,76 @@
+import sys
+
+sys.path.extend([".", ".."])
+
+from utils import *
+
+
+def parse(chunks, extra=0):
+    CM = []
+    for chunk in chunks:
+        a = tuple(int(c.removeprefix("X+").removeprefix("Y+")) for c in chunk[0].split(": ")[1].split(", "))
+        b = tuple(int(c.removeprefix("X+").removeprefix("Y+")) for c in chunk[1].split(": ")[1].split(", "))
+        p = tuple(int(c.removeprefix("X=").removeprefix("Y=")) + extra for c in chunk[2].split(": ")[1].split(", "))
+        CM.append([a, b, p])
+    return CM
+
+
+def p1_bruteforce(data):
+    chunks = get_chunks(data)
+    CM = parse(chunks)
+    ans = 0
+    for cm in CM:
+        min_presses = 0, 0
+        min_cost = 10**9
+        (x1, y1), (x2, y2), (px, py) = cm
+        for pa in range(101):
+            for pb in range(101):
+                cost = 3 * pa + pb
+                if cost > min_cost:
+                    continue
+                if (pa * x1 + pb * x2 == px) and (pa * y1 + pb * y2 == py):
+                    min_presses = pa, pb
+                    min_cost = cost
+        if all(p != 0 for p in min_presses):
+            ans += min_cost
+    return ans
+
+
+def find_presses(CM):
+    ans = 0
+    for cm in CM:
+        (x1, y1), (x2, y2), (px, py) = cm
+        d = x2 * y1 - y2 * x1
+        pb = (px * y1 - py * x1) / d
+        pa = (py * x2 - px * y2) / d
+
+        if pa.is_integer() and pb.is_integer():
+            ans += 3 * int(pa) + int(pb)
+    return ans
+
+
+def p1(data):
+    chunks = get_chunks(data)
+    CM = parse(chunks)
+    return find_presses(CM)
+
+
+def p2(data):
+    chunks = get_chunks(data)
+    CM = parse(chunks, extra=10_000_000_000_000)
+    return find_presses(CM)
+
+
+if __name__ == "__main__":
+    """
+    Given:
+    P_a * x_1 + P_b * x_2 = A_x
+    P_a * y_1 + P_b * y_2 = A_y
+
+    Find P_a and P_b using x, y and A.
+    """
+    data = read_file(f"2024/samples/13.in")
+    data = read_file(f"2024/ins/13.in")
+
+    print(f"part 1: {p1(data)}")
+    print(f"part 2: {p2(data)}")
