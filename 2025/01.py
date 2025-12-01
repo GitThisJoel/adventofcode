8ea2cb361fc56f696e1ddc060b4ea627cc5d8a83
GitThisJoel <joelbacker10@gmail.com>
2025-12-01 14:46:32 +0100

    day 1 done

---
+++ 2025/01.py
@@ -0,0 +1,37 @@
+from utils.parsing import get_lines, read_file
+
+MOD = 100
+
+
+def solve(data, intermediate_zeros: bool) -> int:
+    lines = get_lines(data)
+    pos, ans = 50, 0
+    for line in lines:
+        d, n = line[0], int(line[1:])
+        m = 1 if d == "R" else -1
+        if intermediate_zeros:
+            if m == 1 and pos + n > 99:
+                ans += (pos + n) // MOD
+            elif m == -1 and pos - n <= 0:
+                # '+1' here because '//MOD' do not count the "first time" '0' when moving left
+                ans += (n - pos) // MOD + (1 if pos != 0 else 0)
+        pos = (pos + n * m) % MOD
+        if not intermediate_zeros:
+            ans += 1 if pos == 0 else 0
+    return ans
+
+
+def p1(data) -> int:
+    return solve(data, intermediate_zeros=False)
+
+
+def p2(data) -> int:
+    return solve(data, intermediate_zeros=True)
+
+
+if __name__ == "__main__":
+    data = read_file("2025/samples/01.in")
+    data = read_file("2025/ins/01.in")
+
+    print(f"part 1: {p1(data)}")
+    print(f"part 2: {p2(data)}")
